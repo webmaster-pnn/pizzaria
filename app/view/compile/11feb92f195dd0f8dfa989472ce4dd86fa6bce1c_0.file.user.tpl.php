789cb4f816b77ba14dca3285e891bcc56308a514
webmaster-pnn <52802628+webmaster-pnn@users.noreply.github.com>
2019-08-19 21:38:12 -0300

    adição de paginas
    adição das paginas de cadastro e login

--- app/view/compile/11feb92f195dd0f8dfa989472ce4dd86fa6bce1c_0.file.user.tpl.php
+++ app/view/compile/11feb92f195dd0f8dfa989472ce4dd86fa6bce1c_0.file.user.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.33, created on 2019-08-20 02:19:37
+/* Smarty version 3.1.33, created on 2019-08-20 02:29:11
   from 'C:\xampp\htdocs\pizzaria\app\view\user.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.33',
-  'unifunc' => 'content_5d5b3c9977b4e9_88008628',
+  'unifunc' => 'content_5d5b3ed7ceb9c8_25690138',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '11feb92f195dd0f8dfa989472ce4dd86fa6bce1c' => 
     array (
       0 => 'C:\\xampp\\htdocs\\pizzaria\\app\\view\\user.tpl',
-      1 => 1566260372,
+      1 => 1566260947,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5d5b3c9977b4e9_88008628 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5d5b3ed7ceb9c8_25690138 (Smarty_Internal_Template $_smarty_tpl) {
 ?><main>
 <section>
   <div class="container" >
@@ -51,20 +51,31 @@ function content_5d5b3c9977b4e9_88008628 (Smarty_Internal_Template $_smarty_tpl)
               </div>
               <div class="card-body">
                 
-              <div class="text-center">
-
-                              <h3>
-                                <?php echo $_smarty_tpl->tpl_vars['CLI_NOME']->value;?>
-
-                              </h3>
-                              <h3>
-                                <?php echo $_smarty_tpl->tpl_vars['CLI_SOBRENOME']->value;?>
-
-                              </h3>
-                              
-                </div>
+                <div class="text-center">
               
-              </div>
+                     <div class="form-group col-md-6">
+                                <label for="txtName">Nome</label>
+                                <input type="text" class="form-control" id="txtName" name="txtName" value="<?php echo $_smarty_tpl->tpl_vars['CLI_NOME']->value;?>
+">
+                                <span class="help-block">help</span> 
+                        </div>
+                        <div class="form-group col-md-6">
+                                <label for="txtSobrenome">Sobrenome</label>
+                                <input type="text" class="form-control" id="txtSobrenome" name="txtSobrenome" value="<?php echo $_smarty_tpl->tpl_vars['CLI_SOBRENOME']->value;?>
+">
+                                <span class="help-block">help</span> 
+                         </div>
+
+                         <div class="form-group col-md-12">
+                                <label for="txtEmail">Email</label>
+                                <input type="email" class="form-control" id="txtEmail" name="txtEmail" placeholder="Coloque um email válido" required="required">
+                                <span class="help-block">help</span> 
+                          </div>
+                 
+                              
+                 </div>
+              
+                </div>
             <input type="submit" class="btn btn-sm btn-info mr-4" name="botao" value="Trocar Foto">
             </form>
           </div>
--- app/view/user.tpl
+++ app/view/user.tpl
@@ -25,18 +25,29 @@
               </div>
               <div class="card-body">
                 
-              <div class="text-center">
-
-                              <h3>
-                                {$CLI_NOME}
-                              </h3>
-                              <h3>
-                                {$CLI_SOBRENOME}
-                              </h3>
-                              
-                </div>
+                <div class="text-center">
               
-              </div>
+                     <div class="form-group col-md-6">
+                                <label for="txtName">Nome</label>
+                                <input type="text" class="form-control" id="txtName" name="txtName" value="{$CLI_NOME}">
+                                <span class="help-block">help</span> 
+                        </div>
+                        <div class="form-group col-md-6">
+                                <label for="txtSobrenome">Sobrenome</label>
+                                <input type="text" class="form-control" id="txtSobrenome" name="txtSobrenome" value="{$CLI_SOBRENOME}">
+                                <span class="help-block">help</span> 
+                         </div>
+
+                         <div class="form-group col-md-12">
+                                <label for="txtEmail">Email</label>
+                                <input type="email" class="form-control" id="txtEmail" name="txtEmail" placeholder="Coloque um email válido" required="required">
+                                <span class="help-block">help</span> 
+                          </div>
+                 
+                              
+                 </div>
+              
+                </div>
             <input type="submit" class="btn btn-sm btn-info mr-4" name="botao" value="Trocar Foto">
             </form>
           </div>
